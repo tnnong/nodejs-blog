d1ca99faa378a700328ee6c5295f9d5706c7ee4a
DESKTOP-MG66VR2\PC <nongtranngoc@gmail.com>
2024-01-19 14:52:54 +0700

    reconstruct folder and add public/css folder to gitignore

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Template engine
+app.engine(
+  ".hbs",
+  engine({
+    partialsDir: "src/resources/views/partials",
+    layoutsDir: "src/resources/views/layouts",
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(path.dirname(__dirname), "/src/resources/views"));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
